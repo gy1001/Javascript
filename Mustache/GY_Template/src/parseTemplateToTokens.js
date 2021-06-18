10e719e97e1e30f6308e8d9d97d54f7f2f82cafc
gaoyuan <gaoyuan_flying@163.com>
2021-06-18 15:57:52 +0800

    feat: 增加去除空格函数

--- Mustache/GY_Template/src/parseTemplateToTokens.js
+++ Mustache/GY_Template/src/parseTemplateToTokens.js
@@ -3,14 +3,33 @@ import Scanner from "./Scanner.js"
 export default function parseTemplateToTokens(templateStr){
   // 创建扫描器
     var scanner = new Scanner(templateStr)
-
     var tokens = []
     var word=""
     while(!scanner.eos()){
       word = scanner.scanUtil("{{")
       // 这里可以判断处理一下 空格问题，需要判断处理，例如 <li class="red">这里的空格就不能做处理
       // 增加判断：空格是在 标签中的空格还是 标签间的空格
-      word && tokens.push(['text', word])
+      if(word){
+        let _word=""
+        let isInnerTag = false
+        for (let index = 0; index < word.length; index++) {
+          const element = word[index];
+          if(element === "<"){
+            isInnerTag = true
+          }else if(element === ">"){
+            isInnerTag = false
+          }
+          // 如果当前element 是空格，只有在 isInnerTag 为 true 时候才能加
+          if(/\s/.test(element)){
+            if(isInnerTag){
+              _word += element
+            }
+          }else{
+            _word += element
+          }
+        }
+        tokens.push(['text', _word])
+      }
       scanner.scan("{{")
       word = scanner.scanUtil("}}")
       if(word){
