7c431d0882dfe47eecb945e6140d8d37964b26f4
gaoyuan <gaoyuan_flying@163.com>
2021-07-02 18:05:57 +0800

    feat:增加less支持

--- Typescript/ts-snake/src/index.ts
+++ Typescript/ts-snake/src/index.ts
@@ -1,3 +1,4 @@
+import './style/index.less'
 let a:string = "hello"
 console.log(a+11)
 console.log(Promise)
--- Typescript/ts-snake/webpack.config.js
+++ Typescript/ts-snake/webpack.config.js
@@ -45,6 +45,14 @@ module.exports = {
           'ts-loader'
         ],
         exclude: /node_modules/
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "less-loader"
+        ]
       }
     ]
   },
