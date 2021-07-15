408eca5c751279a240fb517e1658c8204985d9da
gaoyuan <gaoyuan_flying@163.com>
2021-07-15 21:40:51 +0800

    fix:更改错误

--- Typescript/geek-ts/build/webpack.base.config.js
+++ Typescript/geek-ts/build/webpack.base.config.js
@@ -13,7 +13,11 @@ module.exports = {
         test: /\.tsx?$/i,
         use: [
           {
-            loader: 'ts-loader'
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true //  只做语言转换，不做类型检查 ,
+              //编辑器会进行类型提示， 也可以安装一个插件来做类型检查  npm i fork-ts-checker-webpack-plugin -D
+            }
           }
         ],
         exclude: /node_modules/
--- Typescript/geek-ts/build/webpack.config.js
+++ Typescript/geek-ts/build/webpack.config.js
@@ -3,5 +3,11 @@ const baseConfig = require("./webpack.base.config")
 const devConfig = require("./webpack.dev.config")
 const proConfig = require("./webpack.pro.config")
 
-let config = process.NODE_ENV === "development" ? devConfig : proConfig
-module.exports = merge(baseConfig, config)
+// node 环境下不是这样的写法
+//let config = process.env.NODE_ENV === "development" ? devConfig : proConfig
+//module.exports = merge(baseConfig, config)
+
+module.exports = (env, argv) => {
+  let config = argv.mode === "development" ? devConfig : proConfig
+  return merge(baseConfig, config)
+}
