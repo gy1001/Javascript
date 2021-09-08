d34c39585a113c960aa6eba04a43ee98932fca97
gaoyuan <gaoyuan_flying@163.com>
2021-09-09 06:57:44 +0800

    feat:遍历所有的依赖图转换为更好的依赖图表

--- Webpack/mini-webpack/lib/my-webpack/Compiler.js
+++ Webpack/mini-webpack/lib/my-webpack/Compiler.js
@@ -1,19 +1,85 @@
 const {getAst,getCode,getDeps} = require("./parser")
 class Compiler{
   constructor(options){
+    // webpack中 的配置
     this.options = options || {}
+    // 所有依赖容器
+    this.modules = []
   }
   // 启动打包
   run(){
     // 入口文件路径
     const filePath = this.options.entry
+    // 第一次构建，得到入口文件的信息
+    const fileInfo = this.build(filePath)
+    this.modules.push(fileInfo)
+    
+    //  遍历所有的依赖
+    this.modules.forEach((fileInfo) => {
+      // deps 类似结构
+      /*
+        {
+          './add.js': '/Users/gaoyuan/learn/Javascript/Webpack/mini-webpack/src/add.js',
+          './count.js': '/Users/gaoyuan/learn/Javascript/Webpack/mini-webpack/src/count.js'
+        }
+      */
+      // 取出当前文件的所有依赖
+      const hisDeps = fileInfo.deps
+      for (const relativePath in hisDeps) {
+        // 依赖文件的绝对路径
+        const absolutePath = hisDeps[relativePath]
+        // 将依赖文件进行处理
+        const hisFileInfo =  this.build(absolutePath)
+        // 将处理后的文件添加到 modules 中，后面遍历又会进行遍历
+        this.modules.push(hisFileInfo)
+      }
+    })
+
+    // 得到所有的文件依赖
+    //console.log(this.modules)
+    // 将依赖整理成更好的关系依赖图
+    /**
+     {
+       "index.js": {
+         code:'',
+         deps: { 'add.js': "xxx"}
+       },
+       "add.js": {
+         code: "",
+         deps: { }
+       }
+     }
+     */
+
+    const depsGraph = this.modules.reduce((graph,module) => {
+       return {
+         ...graph,
+         [module.filePath]: {
+           code: module.code,
+           deps: module.deps
+         }
+       }
+    }, {})
+    console.log(depsGraph)
+  }
+
+  // 开始构建
+  build(filePath){
     // 1. 将文件解析为ast
     const ast = getAst(filePath)
     // 2. 获取ast中所有的依赖
     const deps = getDeps(ast,filePath)
     // 3. 将ast解析成code
     const code = getCode(ast)
-    console.log(ast,deps,code)
+    // console.log(ast,deps,code)
+    return {
+      // 文件路径
+      filePath,
+      // 当前文件的所有依赖
+      deps,
+      // 当期那文件解析后的代码
+      code
+    }
   }
 }
 
