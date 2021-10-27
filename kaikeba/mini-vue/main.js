9604febdcb1b857740715d8e94df51d3d5a22c6d
gaoyuan <gaoyuan_flying@163.com>
2021-10-27 21:39:15 +0800

    feat:初步实现

--- kaikeba/mini-vue/main.js
+++ kaikeba/mini-vue/main.js
@@ -1,31 +1,35 @@
 console.log('main.js')
-
+// 第一小步
 // 依赖收集 和 触发依赖
-import { ref, effect } from './node_modules/@vue/reactivity/dist/reactivity.esm-browser.js'
+//import { ref, effect } from './node_modules/@vue/reactivity/dist/reactivity.esm-browser.js'
 
-// 组件
-// 响应式数据
-const App = {
-  setup() {
-    // vue3
-    let count = ref(10)
-    window.count = count
-    return {
-      count
-    }
-  },
+//// 组件
+//// 响应式数据
+//const App = {
+//  setup() {
+//    // vue3
+//    let count = ref(10)
+//    window.count = count
+//    return {
+//      count
+//    }
+//  },
 
-  render(setupState) {
-    // 依赖
-    effect(() => {
-      const rootContainer = document.createElement('div')
-      rootContainer.textContent = 'nihao' + setupState.count.value
+//  render(setupState) {
+//    // 依赖
+//    effect(() => {
+//      const rootContainer = document.createElement('div')
+//      rootContainer.textContent = 'nihao' + setupState.count.value
 
-      const app = document.getElementById('app')
-      app.innerHTML = ''
-      app.appendChild(rootContainer)
-    })
-  }
-}
+//      const app = document.getElementById('app')
+//      app.innerHTML = ''
+//      app.appendChild(rootContainer)
+//    })
+//  }
+//}
+//App.render(App.setup())
 
-App.render(App.setup())
+// 第二小步进行升级
+
+import { createApp } from './core/index.js'
+createApp(App).mount('#app')
