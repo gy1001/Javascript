1a7339f2374b3f317c87827a94d4433449cd2ca7
gaoyuan <gaoyuan_flying@163.com>
2021-10-27 21:32:18 +0800

    feat:初步实现组件和组件的响应化

---
+++ kaikeba/mini-vue/main.js
@@ -0,0 +1,31 @@
+console.log('main.js')
+
+// 依赖收集 和 触发依赖
+import { ref, effect } from './node_modules/@vue/reactivity/dist/reactivity.esm-browser.js'
+
+// 组件
+// 响应式数据
+const App = {
+  setup() {
+    // vue3
+    let count = ref(10)
+    window.count = count
+    return {
+      count
+    }
+  },
+
+  render(setupState) {
+    // 依赖
+    effect(() => {
+      const rootContainer = document.createElement('div')
+      rootContainer.textContent = 'nihao' + setupState.count.value
+
+      const app = document.getElementById('app')
+      app.innerHTML = ''
+      app.appendChild(rootContainer)
+    })
+  }
+}
+
+App.render(App.setup())
