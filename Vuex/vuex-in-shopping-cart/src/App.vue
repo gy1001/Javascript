2e5c99a309e49ca2e504c6133d9d07eb3269b041
gaoyuan <gaoyuan_flying@163.com>
2021-11-01 22:54:48 +0800

    feat:购物车样式

--- Vuex/vuex-in-shopping-cart/src/App.vue
+++ Vuex/vuex-in-shopping-cart/src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>购物车示例</h1>
+    <p>账号: {{ email }}</p>
+    <hr />
+    <h2>产品</h2>
+    <ProductList />
+    <hr />
+    <ShoppingCart />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapState } from 'vuex'
+import ProductList from './components/ProductList.vue'
+import ShoppingCart from './components/ShoppingCart.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    ProductList,
+    ShoppingCart
+  },
+  computed: mapState({
+    email: state => state.userInfo.email
+  })
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
