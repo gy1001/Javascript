86028a9460c50df37be19c0d9413b50d013d8698
gaoyuan <gaoyuan_flying@163.com>
2021-11-07 16:59:27 +0800

    feat：本地更改主题

--- Vue/ant-design-vue-pro-demo/vue.config.js
+++ Vue/ant-design-vue-pro-demo/vue.config.js
@@ -2,8 +2,13 @@ module.exports = {
   css: {
     loaderOptions: {
       less: {
-        javascriptEnabled: true
-      }
-    }
-  }
+        javascriptEnabled: true,
+        modifyVars: {
+          'primary-color': '#1DA57A',
+          'link-color': '#1DA57A',
+          'border-radius-base': '2px',
+        },
+      },
+    },
+  },
 }
