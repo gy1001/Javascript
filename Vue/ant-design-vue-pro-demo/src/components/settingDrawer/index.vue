aed385303c9e484758f7b47f15bc17ae5a390efd
gaoyuan <gaoyuan_flying@163.com>
2021-11-07 21:01:33 +0800

    feat:使用插件动态配置主题色

--- Vue/ant-design-vue-pro-demo/src/components/settingDrawer/index.vue
+++ Vue/ant-design-vue-pro-demo/src/components/settingDrawer/index.vue
@@ -31,6 +31,18 @@
         <a-radio value="left"> 左侧 </a-radio>
         <a-radio value="top"> 顶部 </a-radio>
       </a-radio-group>
+      <h2>设置主题</h2>
+      <a-radio-group
+        @change="(e) => handlerThemeChange(e.target.value)"
+        name="navLayout"
+        default-value="#1890ff"
+      >
+        <a-radio value="#1890ff" style="color: #1890ff"> 蓝色 </a-radio>
+        <a-radio value="green" style="color: green"> 绿色 </a-radio>
+        <a-radio value="red" style="color: red"> 红色 </a-radio>
+        <a-radio value="#333" style="color: #333"> 黑色 </a-radio>
+        <a-radio value="purple" style="color: purple"> 紫色 </a-radio>
+      </a-radio-group>
     </a-drawer>
   </div>
 </template>
@@ -41,17 +53,17 @@ export default {
       visible: false,
       navTheme: 'dark',
       navLayout: 'left',
-    };
+    }
   },
   methods: {
-    afterVisibleChange(val) {
-      console.log('visible', val);
+    afterVisibleChange() {
+      //console.log('visible', val)
     },
     showDrawer() {
-      this.visible = true;
+      this.visible = true
     },
     onClose() {
-      this.visible = false;
+      this.visible = false
     },
     handlerSettingChange(type, value) {
       this.$router.push({
@@ -59,23 +71,22 @@ export default {
           ...this.$router.query,
           [type]: value,
         },
-      });
+      })
+    },
+    handlerThemeChange(themeColor) {
+      window.less
+        .modifyVars({
+          '@primary-color': themeColor,
+        })
+        .then(() => {
+          console.log('成功')
+        })
+        .catch((error) => {
+          alert('失败')
+          console.log(error)
+        })
     },
   },
-};
-</script>
-<style lang="less" scoped>
-.setting-handle{
-  position: absolute;
-  top: 240px;
-  right: 300px;
-  width: 48px;
-  height: 48px;
-  background-color: #2f54eb;
-  font-size: 20px;
-  text-align: center;
-  line-height: 48px;
-  border-radius: 3px 0 0 3px;
-  color:#fff
 }
-</style>
+</script>
+<style lang="less" src="./index.less"></style>
--- Vue/ant-design-vue-pro-demo/vue.config.js
+++ Vue/ant-design-vue-pro-demo/vue.config.js
@@ -1,14 +1,35 @@
+const AntDesignThemePlugin = require('antd-theme-webpack-plugin')
+const path = require('path')
+const options = {
+  antDir: path.join(__dirname, './node_modules/ant-design-vue'),
+  stylesDir: path.join(__dirname, './src'),
+  varFile: path.join(
+    __dirname,
+    './node_modules/ant-design-vue/lib/style/themes/default.less'
+  ),
+  lessUrl: 'https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.2/less.min.js',
+  publicPath: '',
+  themeVariables: ['@primary-color'],
+  generateOnce: false,
+}
+
+const themePlugin = new AntDesignThemePlugin(options)
+
 module.exports = {
   css: {
     loaderOptions: {
       less: {
         javascriptEnabled: true,
         modifyVars: {
-          'primary-color': '#1DA57A',
-          'link-color': '#1DA57A',
-          'border-radius-base': '2px',
+          //'primary-color': 'yellow',
+          //'link-color': '#1DA57A',
+          //'border-radius-base': '2px',
         },
       },
     },
+    extract: false,
+  },
+  configureWebpack: {
+    plugins: [themePlugin],
   },
 }
