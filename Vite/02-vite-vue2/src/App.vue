d20c1dfacb5b6f0156927a6e386bd2a8c5fda911
gaoyuan <gaoyuan_flying@163.com>
2021-11-25 19:23:19 +0800

    feat:使用vite运行vue2

---
+++ Vite/02-vite-vue2/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<h1>{{msg}}</h1>
+		<a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			msg: 'hello vue2+vite',
+		}
+	},
+}
+</script>
+
+<style scoped lang="css">
+div {
+	text-align: center;
+}
+</style>
