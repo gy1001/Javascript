9455c21582522178f6d3896d8e74c0bf4d1a90d8
MineMine <gaoyuan_flying@163.com>
2022-03-06 23:22:30 +0800

    doc:其他

--- VuePress/docs/.vuepress/config.ts
+++ VuePress/docs/.vuepress/config.ts
@@ -3,10 +3,13 @@ import type { DefaultThemeOptions } from 'vuepress'
 import { sidebarZh } from '../.vuepress/configs/siderbar/zh'
 
 export default defineUserConfig<DefaultThemeOptions>({
+	head: [
+		['meta', { name: 'referrer', content: 'no-referrer' }],
+	],
 	// 站点配置
 	lang: 'en-US',
 	title: 'gyfly.top',
-	description: '文章杂谈',
+	description: '随便写写',
 	theme: '@vuepress/theme-default',
 	themeConfig: {
 		lastUpdated: true,
@@ -23,7 +26,7 @@ export default defineUserConfig<DefaultThemeOptions>({
 			},
 			{
 				text: '其他',
-				link: '/other',
+				link: '/other/',
 			},
 			{
 				text: '休闲娱乐',
@@ -41,5 +44,21 @@ export default defineUserConfig<DefaultThemeOptions>({
 			},
 		},
 	},
-	plugins: ['@vuepress/nprogress', '@vuepress/back-to-top']
+	plugins: [
+		['@vuepress/nprogress', true],
+		['@vuepress/back-to-top', true],
+		[
+			'vuepress-plugin-comment',
+			{
+				choosen: 'valine',
+				// options选项中的所有参数，会传给Valine的配置
+				options: {
+					el: '#valine-vuepress-comment',
+					appId: 'G2osIAGrkQkOPu3B17DR2lAQ-gzGzoHsz',
+					appKey: 'cfCR0oqsDLtt6zcIzCoHOhrX',
+					placeholder: '同道中人，文明留言...',  // 评论框占位提示符
+					lang: 'zh-cn', // 支持中文
+				}
+			}
+		]]
 })
