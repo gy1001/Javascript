bf45951c1ce165d00ad3cb5221cad1d234b1eb07
MineMine <gaoyuan_flying@163.com>
2022-03-07 19:22:05 +0800

    feat:增加复制代码功能

--- VuePress/docs/.vuepress/config.ts
+++ VuePress/docs/.vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
 import { sidebarZh } from '../.vuepress/configs/siderbar/zh'
-
+import { copyCode } from "vuepress-plugin-copy-code2";
 export default defineUserConfig<DefaultThemeOptions>({
 	head: [
 		['meta', { name: 'referrer', content: 'no-referrer' }],
@@ -48,18 +48,15 @@ export default defineUserConfig<DefaultThemeOptions>({
 	plugins: [
 		['@vuepress/nprogress', true],
 		['@vuepress/back-to-top', true],
-		[
-			'vuepress-plugin-comment',
-			{
-				choosen: 'valine',
-				// options选项中的所有参数，会传给Valine的配置
-				options: {
-					el: '#valine-vuepress-comment',
-					appId: 'G2osIAGrkQkOPu3B17DR2lAQ-gzGzoHsz',
-					appKey: 'cfCR0oqsDLtt6zcIzCoHOhrX',
-					placeholder: '同道中人，文明留言...',  // 评论框占位提示符
-					lang: 'zh-cn', // 支持中文
+		copyCode({
+			showInMobile: true,
+			pure: false,
+			locales: {
+				'/': {
+					copy: '复制',
+					hint: '复制成功'
 				}
 			}
-		]]
+		})
+	]
 })
