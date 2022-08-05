c7a2f9f5cc02785fdd7c5d97ae4fae480aca3637
高原 <gaoyuan200408@credithc.com>
2022-08-05 19:00:15 +0800

    feat:贪吃蛇动画

--- Typescript/尚硅谷-ts/snake-ts/src/modules/GameControl.ts
+++ Typescript/尚硅谷-ts/snake-ts/src/modules/GameControl.ts
@@ -3,7 +3,10 @@ import Snake from './Snake'
 class GameControl {
   food: Food
   snake: Snake
-
+  // 用来存储蛇运动的方向
+  direction: string = 'ArrowDown'
+  // 游戏是否结束
+  isLive: boolean = true
   constructor() {
     console.log('游戏开始初始化')
     this.food = new Food()
@@ -11,7 +14,66 @@ class GameControl {
     this.init()
   }
 
-  init() {}
+  init() {
+    // 绑定事件
+    document.addEventListener('keydown', this.keyDownHandler.bind(this))
+    this.run()
+  }
+
+  keyDownHandler(event: KeyboardEvent) {
+    this.direction = event.key
+  }
+
+  run() {
+    let x = this.snake.X
+    let y = this.snake.Y
+    switch (this.direction) {
+      case 'ArrowDown':
+      case 'Down':
+        y += 10
+        break
+      case 'ArrowLeft':
+      case 'Left':
+        x -= 10
+        break
+      case 'ArrowRight':
+      case 'Right':
+        x += 10
+        break
+      case 'ArrowUp':
+      case 'Up':
+        y -= 10
+        break
+      default:
+        break
+    }
+    this.checkEat(x, y)
+    try {
+      this.snake.X = x
+      this.snake.Y = y
+    } catch (error) {
+      console.log(error)
+      this.isLive = false
+    }
+
+    // 开启一个定时调用
+    if (this.isLive) {
+      setTimeout(() => {
+        this.run()
+      }, 300)
+    }
+  }
+
+  checkEat(x: number, y: number) {
+    if (x === this.food.X && y === this.food.Y) {
+      console.log('吃到食物了')
+      // 食物的位置改变
+      this.food.change()
+      // 分数 +1
+      // 蛇的 body 加1
+      this.snake.addBodies()
+    }
+  }
 }
 
 export default GameControl
--- Typescript/尚硅谷-ts/snake-ts/src/modules/Snake.ts
+++ Typescript/尚硅谷-ts/snake-ts/src/modules/Snake.ts
@@ -1,20 +1,20 @@
 class Snake {
-  head: HTMLElement | null
-  snake: HTMLElement | null
-  bodies: HTMLCollection | undefined
+  head: HTMLElement
+  snake: HTMLElement
+  bodies: HTMLCollection
 
   constructor() {
-    this.snake = document.getElementById('snake')
-    this.head = document.querySelector('#snake>div')
-    this.bodies = this.snake?.getElementsByTagName('div')
+    this.snake = document.getElementById('snake') as HTMLElement
+    this.head = document.querySelector('#snake>div') as HTMLElement
+    this.bodies = this.snake.getElementsByTagName('div') as HTMLCollection
   }
 
   get X() {
-    return this.head?.offsetLeft
+    return this.head.offsetLeft
   }
 
   get Y() {
-    return this.head?.offsetTop
+    return this.head.offsetTop
   }
 
   gameOver() {
@@ -23,10 +23,56 @@ class Snake {
 
   set X(value) {
     console.log('设置蛇的x的值')
+    if (value < 0 || value > 290) {
+      this.gameOver()
+      return
+    }
+    this.moveBody()
+    this.head.style.left = value + 'px'
+    this.checkHeadBody()
   }
 
   set Y(value) {
     console.log('设置蛇的y的值')
+    // if (this.Y === value) {
+    //   return
+    // }
+    if (value < 0 || value > 290) {
+      this.gameOver()
+      return
+    }
+    this.head.style.top = value + 'px'
+    // if (this.bodies.length > 1) {
+    // }
+  }
+
+  addBodies() {
+    const div = document.createElement('div')
+    this.snake.insertAdjacentElement('beforeend', div)
+  }
+
+  moveBody() {
+    for (let index = this.bodies.length - 1; index > 0; index--) {
+      const element = this.bodies[index]
+      // 获取前面身体的位置
+      const hisFirstEl = this.bodies[index - 1]
+      let X = (hisFirstEl as HTMLElement).offsetLeft
+      let Y = (hisFirstEl as HTMLElement).offsetTop
+      // 把这个值设置到当前值身上
+      ;(element as HTMLElement).style.left = X + 'px'
+      ;(element as HTMLElement).style.top = Y + 'px'
+    }
+  }
+
+  // 检测头部有没有碰撞到身体
+  checkHeadBody() {
+    for (let index = this.bodies.length - 1; index > 0; index--) {
+      const currentEl = this.bodies[index] as HTMLElement
+      if (this.X === currentEl.offsetLeft && this.Y === currentEl.offsetTop) {
+        // 蛇头撞到身体了
+        throw new Error('蛇头撞到身体了')
+      }
+    }
   }
 }
 
