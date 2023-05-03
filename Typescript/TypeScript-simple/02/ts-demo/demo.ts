68960b82c20b6c1614ccf2fff6a12627fed9370f
MineMine <gaoyuan_flying@163.com>
2023-05-03 23:37:51 +0800

    类和继承

--- Typescript/TypeScript-simple/02/ts-demo/demo.ts
+++ Typescript/TypeScript-simple/02/ts-demo/demo.ts
@@ -254,10 +254,256 @@ const box: Box<string> = { content: 'box' }
 const box1: Box<number> = { content: 100 }
 const box2: Box<boolean> = { content: true }
 
-type orNull<Type> = Type | null
-const test4: orNull<string> = '111'
-const test5: orNull<string> = null
+type OrNull<Type> = Type | null
+const test4: OrNull<string> = '111'
+const test5: OrNull<string> = null
 
-type oneOrMany<T> = T | T[]
-const test6: oneOrMany<string> = ['123']
-const test7: oneOrMany<string> = '123'
+type OneOrMany<T> = T | T[]
+const test6: OneOrMany<string> = ['123']
+const test7: OneOrMany<string> = '123'
+
+type OneOrManyOrNull<T> = OrNull<OneOrMany<T>>
+const test8: OneOrManyOrNull<string> = null
+const test9: OneOrManyOrNull<string> = '123'
+const test10: OneOrManyOrNull<string> = ['123']
+
+// 数组和泛型
+interface SelfArray<T> {
+  [key: number]: T
+  length: number
+  pop(): T | undefined
+  push(...items: T[]): number
+}
+const NumberArr: Array<number> = [1, 2, 3, 4]
+const test11: SelfArray<number> = [2, 3, 4, 5]
+
+// 数组的 readOnly 修饰符
+function doStuff(arr: readonly string[]) {
+  arr.slice()
+  // 使用push 会改变原数组，ts 会报错
+}
+
+// 元组
+type Tuple = [number, string]
+const tuple: Tuple = [1, '2']
+
+type PointTest = [number, number]
+function getPointTest([x, y]: PointTest) {
+  return x + y
+}
+const point: PointTest = [1, 2]
+getPointTest(point)
+
+// 泛型中使用extends和keyof语法
+interface Peroson {
+  name: string
+}
+function getName(person: Peroson) {
+  return person.name
+}
+getName({ name: '孙悟空' })
+getName({ name: '孙悟空', age: 100 }) // 就会报错
+
+// 可以进行如下修改
+function getNameNew<T extends Peroson>(person: T) {
+  return person.name
+}
+getNameNew({ name: '孙悟空' })
+getNameNew({ name: '孙悟空', age: 100 }) // 不会报错
+
+// keyof
+interface Teacher {
+  name: string
+  age: number
+  sex: 'male' | 'female'
+}
+const teacher: Teacher = {
+  name: '唐僧',
+  age: 100,
+  sex: 'male',
+}
+function getTeacherInfo<T extends keyof Teacher>(teacher: Teacher, key: T) {
+  return teacher[key]
+}
+
+getTeacherInfo(teacher, 'def') // 这里乱写就会报错，只能是 Teacher 类型的 key 值
+getTeacherInfo(teacher, 'name')
+
+// 条件类型
+// 是根据条件，生成一个新的类型
+interface AnimalTest {
+  breath: () => {}
+}
+interface DogTest extends AnimalTest {
+  bark: () => {}
+}
+interface Tank {
+  ph: number
+}
+
+type Example = Dog extends Animal ? string : number
+// 使用条件类型的例子
+interface IdLable {
+  id: number
+}
+interface NameLabel {
+  name: string
+}
+//当然使用函数重载一样可以实现，不过比较麻烦，如下
+// function createLabel(key: string): IdLable
+// function createLabel(key: number): NameLabel
+// function createLabel(key: string | number): IdLable | NameLabel {
+//   if (typeof key === 'string') {
+//     return { name: key }
+//   }
+//   return { id: key }
+// }
+// const label = createLabel('孙悟空')
+
+type IdOrNameLable<T> = T extends number ? IdLable : NameLabel
+function createLabel<T extends string | number>(key: T): IdOrNameLable<T>
+function createLabel(key: string | number): IdLable | NameLabel {
+  if (typeof key === 'string') {
+    return { name: key }
+  }
+  return { id: key }
+}
+const labelString = createLabel('孙悟空')
+const labelNumber = createLabel(1122)
+
+type TypeOfMessageOf<T> = T extends { message: unknown } ? T['message'] : never
+type Message = TypeOfMessageOf<{ message: string }> // 此时 Type Message 是 string 类型
+type Messag1 = TypeOfMessageOf<string> // 此时 Type Message1 是 never 类型
+
+interface Email {
+  from: string
+  to: string
+  message: string
+}
+
+// type EmailMessage = string
+
+const emailObject: Email = {
+  from: 'from@qq.com',
+  to: 'to@qq.com',
+  message: 'hello i send a message to you',
+}
+const email: TypeOfMessageOf<Email> = 'hello it is a message'
+
+type GetReturnType<T> = T extends (...args: never[]) => infer ReturnType
+  ? ReturnType
+  : never
+
+type ExampleType = GetReturnType<() => string> // 推断出 ExampleType 是 string
+type ExampleType2 = GetReturnType<string> // 推断出 ExampleType 是 never
+
+type ToArray<Type> = Type extends any ? Type[] : never
+type StringArray = ToArray<string> // 推断出 StringArray 类型是 string[]
+type StringArray1 = ToArray<string | number> // 推断出 StringArray1 类型是 number[] | string[]
+type StringArrray2 = ToArray<never> // 推断出 StringArray2 类型是 never
+
+type ToArrayTest<T> = [T] extends [any] ? T[] : never
+type StringOrNumberArray = ToArrayTest<string | number> // 推断出 StringArray 类型是 (string | number)[]
+
+// 映射类型
+interface User {
+  name: string
+  age: number
+  male: boolean
+}
+
+// type DeleteMaleProperty<T> = {
+//   [Property in keyof T as Exclude<Property, 'male'>]: T[Property]
+// }
+
+// type UserWithoutGender = DeleteMaleProperty<User> // 类型目前是 { name: string, age: number }
+
+// const user: UserWithoutGender = { name: '孙悟空', age: 500 }
+
+// 想要构建如下的类型，
+// interface UserFunctions {
+//   getName: () => string
+//   getAge: () => number
+//   getMale: () => boolean
+// }
+
+//Capitalize: 转换字符串字面量的第一个字母为大写字母
+type GetPropertyFunctions<T> = {
+  [Property in keyof T as `get${Capitalize<
+    string & Property
+  >}`]: () => T[Property]
+}
+
+type UserFunctions = GetPropertyFunctions<User>
+
+type SquareEvent = {
+  kind: 'square'
+  x: number
+  y: number
+}
+
+type CircleEvent = {
+  kind: 'circle'
+  radius: number
+}
+
+type GenerateEventsFunctions<T extends { kind: string }> = {
+  [Event in T as Event['kind']]: (event: Event) => void
+}
+
+type NewType = GenerateEventsFunctions<SquareEvent | CircleEvent>
+// 此时 NewType 类型为 { square: (event: SquareEvent) => void; circle: (event: CircleEvent) => void; }
+
+class PersonTest {
+  name: string = '孙悟空'
+  getName() {
+    return this.name
+  }
+}
+const person = new PersonTest()
+console.log(person.getName())
+
+class Teacher extends PersonTest {
+  getTeacherName() {
+    return '唐僧'
+  }
+  // 也可以对同名方法进行重写
+  getName() {
+    const name = super.getName() + '和' + '金蝉子'
+    return name
+  }
+}
+
+const teacherTest = new Teacher()
+console.log(teacherTest.getName())
+console.log(teacherTest.getTeacherName())
+// 类中的访问类型和构造器
+// private protected public 访问类型
+// public: 允许我在类的内外被调用
+// class PersonTestTwo {
+//   //  传统写法
+//   // public name: string
+//   // constructor(name: string) {
+//   //   this.name = name
+//   // }
+//   // 简化写法
+//   constructor(public name: string) {
+//     this.name = name
+//   }
+// }
+// const perosonTestTwo = new PersonTestTwo('猪八戒')
+// console.log(perosonTestTwo.name)
+
+class PersonTwo {
+  constructor(public name: string) {}
+}
+
+class TeacherTestTwo extends PersonTwo {
+  constructor(public age: number) {
+    super('teacher')
+    this.age = age
+  }
+}
+const teacherTestTwo = new TeacherTestTwo(28)
+console.log(teacherTestTwo.name)
+console.log(teacherTestTwo.age)
