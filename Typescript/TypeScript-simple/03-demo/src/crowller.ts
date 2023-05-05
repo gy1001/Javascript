8d6a77054d8ab18a17ee86d70aa50c430e05b265
MineMine <gaoyuan_flying@163.com>
2023-05-05 22:50:43 +0800

    使用cheerio进行数据提取

--- Typescript/TypeScript-simple/03-demo/src/crowller.ts
+++ Typescript/TypeScript-simple/03-demo/src/crowller.ts
@@ -1,15 +1,40 @@
 import superagent from 'superagent'
+import cheerio from 'cheerio'
+
+interface CourseInfo {
+  title: string
+  count: number
+}
+
 class Crowller {
   private sercret = 'serretKey'
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.sercret}`
-  private rawHtml = ''
   constructor() {
     this.getRawHtml()
   }
   async getRawHtml() {
     const result = await superagent.get(this.url)
-    this.rawHtml = result.text
-    console.log(this.rawHtml)
+    this.getCourseInfo(result.text)
+  }
+
+  getCourseInfo(html: string) {
+    const $ = cheerio.load(html)
+    const courseItems = $('.course-item')
+    const courseInfos: Array<CourseInfo> = []
+    courseItems.map((index, element) => {
+      const descs = $(element).find('.course-desc')
+      const title = descs.eq(0).text()
+      const count = parseInt(descs.eq(1).text().split('：')[1], 10)
+      courseInfos.push({
+        title,
+        count,
+      })
+    })
+    const result = {
+      time: new Date().getTime(),
+      data: courseInfos,
+    }
+    console.log(result)
   }
 }
 
