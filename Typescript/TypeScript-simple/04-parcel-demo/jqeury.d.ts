79f1149a0a95fa87910bf15562f42f6ee818dc79
MineMine <gaoyuan_flying@163.com>
2023-05-07 16:51:52 +0800

    泛型中keyof语法的使用

--- Typescript/TypeScript-simple/04-parcel-demo/jqeury.d.ts
+++ Typescript/TypeScript-simple/04-parcel-demo/jqeury.d.ts
@@ -23,16 +23,18 @@
 // }
 
 // ES6  模块化
-declare module 'jquery' {
-  interface JqueryInstance {
-    html: (html: string) => JqueryInstance
-  }
-  declare function $(readyFunc: () => void): void
-  declare function $(selector: string): JqueryInstance
-  declare namespace $ {
-    namespace fn {
-      class init {}
-    }
-  }
-  export = $
-}
+// declare module 'jquery' {
+//   interface JqueryInstance {
+//     html: (html: string) => JqueryInstance
+//   }
+//   declare function $(readyFunc: () => void): void
+//   declare function $(selector: string): JqueryInstance
+//   declare namespace $ {
+//     namespace fn {
+//       class init {}
+//     }
+//   }
+//   export = $
+// }
+
+ecl
--- Typescript/TypeScript-simple/04-parcel-demo/src/page.ts
+++ Typescript/TypeScript-simple/04-parcel-demo/src/page.ts
@@ -1,9 +1,29 @@
-const teacher: string = '唐僧'
-console.log(teacher)
+// const teacher: string = '唐僧'
+// console.log(teacher)
 
-import $ from 'jquery'
+// import $ from 'jquery'
 
-$(function () {
-  $('body').html('<h1>123</h1>')
-  new $.fn.init()
+// $(function () {
+//   $('body').html('<h1>123</h1>')
+//   new $.fn.init()
+// })
+
+interface Person {
+  name: string
+  age: number
+  gender: string
+}
+class Teacher {
+  constructor(private info: Person) {}
+
+  getInfo<T extends keyof Person>(key: T) {
+    return this.info[key]
+  }
+}
+const teacher = new Teacher({
+  name: '唐僧',
+  age: 100,
+  gender: 'male',
 })
+const test = teacher.getInfo('name')
+console.log(test)
