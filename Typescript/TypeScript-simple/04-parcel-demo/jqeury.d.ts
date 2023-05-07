87e0a88ebe148cb5afd1c210230b5e5b3b8d466c
MineMine <gaoyuan_flying@163.com>
2023-05-07 16:31:59 +0800

    模块代码的类型描述文件

--- Typescript/TypeScript-simple/04-parcel-demo/jqeury.d.ts
+++ Typescript/TypeScript-simple/04-parcel-demo/jqeury.d.ts
@@ -1,23 +1,38 @@
-interface JqueryInstance {
-  html: (html: string) => JqueryInstance
-}
-
-// 定义全局变量
-// declare var $: (param: () => void) => void
-
-// 定义全局函数
-declare function $(readyFunc: () => void): void
-declare function $(selector: string): JqueryInstance
-
-// interface JQuery {
-//   (readyFunc: () => void): void
-//   (selector: string): JqueryInstance
+// interface JqueryInstance {
+//   html: (html: string) => JqueryInstance
 // }
 
-// declare var $: JQuery
+// // 定义全局变量
+// // declare var $: (param: () => void) => void
 
-declare namespace $ {
-  namespace fn {
-    class init {}
+// // 定义全局函数
+// declare function $(readyFunc: () => void): void
+// declare function $(selector: string): JqueryInstance
+
+// // interface JQuery {
+// //   (readyFunc: () => void): void
+// //   (selector: string): JqueryInstance
+// // }
+
+// // declare var $: JQuery
+
+// declare namespace $ {
+//   namespace fn {
+//     class init {}
+//   }
+// }
+
+// ES6  模块化
+declare module 'jquery' {
+  interface JqueryInstance {
+    html: (html: string) => JqueryInstance
   }
+  declare function $(readyFunc: () => void): void
+  declare function $(selector: string): JqueryInstance
+  declare namespace $ {
+    namespace fn {
+      class init {}
+    }
+  }
+  export = $
 }
--- Typescript/TypeScript-simple/04-parcel-demo/src/page.ts
+++ Typescript/TypeScript-simple/04-parcel-demo/src/page.ts
@@ -1,6 +1,8 @@
 const teacher: string = '唐僧'
 console.log(teacher)
 
+import $ from 'jquery'
+
 $(function () {
   $('body').html('<h1>123</h1>')
   new $.fn.init()
