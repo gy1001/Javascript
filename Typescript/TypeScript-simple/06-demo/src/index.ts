c8051ed4b15db8c973dabfdc0641b034bdc88c86
MineMine <gaoyuan_flying@163.com>
2023-05-08 17:20:05 +0800

    访问器的装饰器

--- Typescript/TypeScript-simple/06-demo/src/index.ts
+++ Typescript/TypeScript-simple/06-demo/src/index.ts
@@ -54,33 +54,61 @@
 // 方法装饰器
 // 普通方法中，target 对应的是 property, key: 装饰的方法的名字
 // 静态方法中，target 对应的是类的构造函数
-function getNameDecerator(
+// function getNameDecerator(
+//   target: any,
+//   key: string,
+//   descriptor: PropertyDescriptor,
+// ) {
+//   // 不允许被重写
+//   descriptor.writable = false
+//   descriptor.value = function () {
+//     return '我是新的值'
+//   }
+// }
+
+// class Test {
+//   name: string
+//   constructor(name: string) {
+//     this.name = name
+//   }
+//   @getNameDecerator
+//   getName() {
+//     return this.name
+//   }
+// }
+// const test = new Test('唐僧')
+
+// console.log(test.getName()) // 我是新的值
+// // 装饰器中设置 descriptor.writable = false 再次运行就会如下错误
+// // Cannot assign to read only property 'getName' of object '#<Test>'
+// // test.getName = function () {
+// //   return '23333'
+// // }
+
+// 访问器装饰器
+function visiDecorator(
   target: any,
   key: string,
   descriptor: PropertyDescriptor,
 ) {
-  // 不允许被重写
   descriptor.writable = false
-  descriptor.value = function () {
-    return '我是新的值'
-  }
 }
 
 class Test {
-  name: string
+  private _name: string
   constructor(name: string) {
-    this.name = name
+    this._name = name
   }
-  @getNameDecerator
-  getName() {
-    return this.name
+  get name() {
+    return this._name
+  }
+  @visiDecorator
+  set name(name: string) {
+    this._name = name
   }
 }
-const test = new Test('唐僧')
 
-console.log(test.getName()) // 我是新的值
-// 装饰器中设置 descriptor.writable = false 再次运行就会如下错误
-// Cannot assign to read only property 'getName' of object '#<Test>'
-// test.getName = function () {
-//   return '23333'
-// }
+const test = new Test('唐僧')
+// 此时因为 descriptor.writable = false 所以这里运行会报错
+// test.name = '1123'
+console.log(test.name) // 1123
