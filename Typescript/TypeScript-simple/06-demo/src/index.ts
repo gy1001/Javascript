a801c197a9a3ca0a20912451bee42eeebc466317
MineMine <gaoyuan_flying@163.com>
2023-05-08 16:29:12 +0800

    类的装饰器(下)

--- Typescript/TypeScript-simple/06-demo/src/index.ts
+++ Typescript/TypeScript-simple/06-demo/src/index.ts
@@ -8,22 +8,45 @@
 //   console.log('testDecoratorTwo')
 // }
 
-function shouleRunDerector(flag: boolean) {
-  if (flag) {
-    return function testDecorator(constructor: any) {
-      console.log('testDecorator')
+// function shouleRunDerector(flag: boolean) {
+//   if (flag) {
+//     return function testDecorator(constructor: any) {
+//       console.log('testDecorator')
+//     }
+//   } else {
+//     return function testDecorator() {}
+//   }
+// }
+
+// // @testDecorator
+// // @testDecoratorTwo
+// @shouleRunDerector(true)
+// class Test {}
+
+// @shouleRunDerector(false)
+// class TestTwo {}
+
+// const test = new Test()
+
+// 第二部分
+function testDecorator<T extends { new (...args: any[]): {} }>(constructor: T) {
+  return class extends constructor {
+    name = '菩提老祖'
+    age = 100
+    getName() {
+      return this.name
     }
-  } else {
-    return function testDecorator() {}
   }
 }
 
-// @testDecorator
-// @testDecoratorTwo
-@shouleRunDerector(true)
-class Test {}
+@testDecorator
+class Test {
+  name: string
+  constructor(name: string) {
+    this.name = name
+  }
+}
 
-@shouleRunDerector(false)
-class TestTwo {}
-
-const test = new Test()
+const test = new Test('唐僧')
+console.log(test) // Test { name: '菩提老祖', age: 100 }
+console.log((test as any).getName()) // 菩提老祖
