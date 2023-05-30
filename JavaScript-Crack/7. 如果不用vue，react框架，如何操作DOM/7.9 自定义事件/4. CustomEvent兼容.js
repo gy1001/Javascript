acd7bfbb907bc950d49e4c6f8ce2b1899702f9eb
MineMine <gaoyuan_flying@163.com>
2023-05-30 16:40:14 +0800

    : 自定义事件，满足个性化需求，增加代码灵活度

--- JavaScript-Crack/7. 如果不用vue，react框架，如何操作DOM/7.9 自定义事件/4. CustomEvent兼容.js
+++ JavaScript-Crack/7. 如果不用vue，react框架，如何操作DOM/7.9 自定义事件/4. CustomEvent兼容.js
@@ -1,21 +1,26 @@
+;(function () {
+  if (typeof CustomEvent !== 'function') {
+    var CustomEvent = function (event, params) {
+      params = params || {
+        bubbles: false,
+        cancelable: false,
+        detail: undefined,
+      }
 
+      var evt = document.createEvent('CustomEvent')
 
+      evt.initCustomEvent(
+        event,
+        params.bubbles,
+        params.cancelable,
+        params.detail,
+      )
 
+      return evt
+    }
 
-(function(){    
-    if(typeof CustomEvent !== "function"){
-     var CustomEvent = function(event, params){
-         params = params || { bubbles: false, cancelable: false, detail: undefined };
+    CustomEvent.prototype = window.Event.prototype
 
-         var evt = document.createEvent('CustomEvent');
-
-         evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-
-         return evt;
-     };
-
-     CustomEvent.prototype = window.Event.prototype;
-
-     window.CustomEvent = CustomEvent;
- }
-})();
+    window.CustomEvent = CustomEvent
+  }
+})()
