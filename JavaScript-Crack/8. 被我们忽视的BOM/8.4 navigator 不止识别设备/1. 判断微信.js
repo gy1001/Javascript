b7cc497d003345785bffaf01656e8181c6bf813f
MineMine <gaoyuan_flying@163.com>
2023-06-01 00:09:53 +0800

    navigator，可不止步于设备识别

--- JavaScript-Crack/8. 被我们忽视的BOM/8.4 navigator 不止识别设备/1. 判断微信.js
+++ JavaScript-Crack/8. 被我们忽视的BOM/8.4 navigator 不止识别设备/1. 判断微信.js
@@ -5,13 +5,10 @@
 //mozilla/5.0 (iphone; cpu iphone os 5_1_1 like mac os x) applewebkit/534.46 (khtml, like gecko) mobile/9b206 micromessenger/5.0
 
 function isWX() {
-    var ua = window.navigator.userAgent.toLowerCase();
-    return ua.match(/MicroMessenger/i) == "micromessenger";
+  var ua = window.navigator.userAgent.toLowerCase()
+  return ua.match(/MicroMessenger/i) == 'micromessenger'
 }
 
-
-
-
-if ("serviceWorker" in navigator) {
-    // Supported!
+if ('serviceWorker' in navigator) {
+  // Supported!
 }
--- JavaScript-Crack/8. 被我们忽视的BOM/8.4 navigator 不止识别设备/5.1 server.js
+++ JavaScript-Crack/8. 被我们忽视的BOM/8.4 navigator 不止识别设备/5.1 server.js
@@ -1,16 +1,14 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
-
-app.use("/report/xhr", function(req, res, next) {
-    console.log("收到上报请求, from xhr");
+app.use('/report/xhr', function (req, res, next) {
+  console.log('收到上报请求, from xhr')
 })
-app.use("/report/bean", function(req, res, next) {
-    console.log("收到上报请求, from bean");
+app.use('/report/bean', function (req, res, next) {
+  console.log('收到上报请求, from bean')
 })
 
-
-app.listen(3000, function(){
-    console.log('listening port 3000')
+app.listen(3000, function () {
+  console.log('listening port 3000')
 })
