7f3d3ddac700abd15760c22cf7f80ddb414b5e48
MineMine <gaoyuan_flying@163.com>
2023-06-01 10:21:36 +0800

    history，网页端的方向盘

--- JavaScript-Crack/8. 被我们忽视的BOM/8.5 history/server.js
+++ JavaScript-Crack/8. 被我们忽视的BOM/8.5 history/server.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const path =require('path')
+const express = require('express')
+const path = require('path')
 
-const app = express();
+const app = express()
 
-
-app.get("*", function(req, res, next) {
-    res.sendFile(path.join(__dirname, "./3.static/index.html"));
+app.get('*', function (req, res, next) {
+  res.sendFile(path.join(__dirname, './3.static/index.html'))
 })
 
-
-app.listen(8086, function() {
-    console.log("listening on port 8086")
+app.listen(8086, function () {
+  console.log('listening on port 8086')
 })
