32b3d8120c0e4a2e76da8e855f6be155b0a81a85
MineMine <gaoyuan_flying@163.com>
2023-06-05 23:46:40 +0800

    Cookie

--- JavaScript-Crack/12. 客户端存储/1. cookie/CookieStoreManager/sw.js
+++ JavaScript-Crack/12. 客户端存储/1. cookie/CookieStoreManager/sw.js
@@ -1,22 +1,18 @@
+self.addEventListener('install', async (event) => {
+  console.log('service worker is installed')
+  const subscriptions = await self.registration.cookies.getSubscriptions()
+  await self.registration.cookies.unsubscribe(subscriptions)
 
-
-
-
-self.addEventListener("install", async (event) => {
-    console.log("service worker is installed");
-    const subscriptions = await self.registration.cookies.getSubscriptions();
-    await self.registration.cookies.unsubscribe(subscriptions);
-
-    await self.registration.cookies.subscribe([
-        {
-            name: "cookie-x",
-        },
-    ]);
-});
+  await self.registration.cookies.subscribe([
+    {
+      name: 'cookie-x',
+    },
+  ])
+})
 
 // 监听变化
-self.addEventListener("cookiechange", (ev) => {
-    console.log("service worker cookiechange:", ev.changed, ev.deleted);
-});
+self.addEventListener('cookiechange', (ev) => {
+  console.log('service worker cookiechange:', ev.changed, ev.deleted)
+})
 
-console.log("service worker !!!!");
+console.log('service worker !!!!')
