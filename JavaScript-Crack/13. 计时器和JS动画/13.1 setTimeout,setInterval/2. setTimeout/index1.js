8a7face20772e72ee1ab2b2d0a782badc2e9e826
MineMine <gaoyuan_flying@163.com>
2023-06-07 00:00:53 +0800

    计时器和JS动画

--- JavaScript-Crack/13. 计时器和JS动画/13.1 setTimeout,setInterval/2. setTimeout/index1.js
+++ JavaScript-Crack/13. 计时器和JS动画/13.1 setTimeout,setInterval/2. setTimeout/index1.js
@@ -1,19 +1,18 @@
 //设置最大值
 setTimeout(() => {
-    console.log("a");
-}, 2 ** 31);
+  console.log('a')
+}, 2 ** 31)
 
 //设置最小值
 setTimeout(() => {
-    console.log("b");
-}, 1);
+  console.log('b')
+}, 1)
 
 setTimeout(() => {
-    console.log("c");
-}, 0.5);
+  console.log('c')
+}, 0.5)
 
-//设置0 
+//设置0
 setTimeout(() => {
-    console.log("d");
-}, 0);
-
+  console.log('d')
+}, 0)
