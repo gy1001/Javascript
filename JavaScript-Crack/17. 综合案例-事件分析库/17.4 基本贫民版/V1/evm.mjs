f4b8c1850efd306b98d531f8d9ef4d2dfc4e03de
MineMine <gaoyuan_flying@163.com>
2023-06-09 11:59:35 +0800

    事件分析库

--- JavaScript-Crack/17. 综合案例-事件分析库/17.4 基本贫民版/V1/evm.mjs
+++ JavaScript-Crack/17. 综合案例-事件分析库/17.4 基本贫民版/V1/evm.mjs
@@ -1,83 +1,85 @@
-
-
 // 保留原始的原型
-const orgEventTargetPro = { ...EventTarget.prototype };
+const orgEventTargetPro = { ...EventTarget.prototype }
 
-const ep = EventTarget.prototype;
+const ep = EventTarget.prototype
 
 // Proxy.revocable返回值
-let rvAdd, rvRemove;
+let rvAdd, rvRemove
 
 // 是否已监听
-let isWatched = false;
+let isWatched = false
 // 默认的回调
-let _callback = () => { };
+let _callback = () => {}
 
 /**
- * 
+ *
  * 创建可撤销的代理对象
- * @param {any} obj 
- * @param {any} handler 
- * @returns 
+ * @param {any} obj
+ * @param {any} handler
+ * @returns
  */
 function createRevocableProxy(obj, handler) {
-    return Proxy.revocable(obj, handler);
+  return Proxy.revocable(obj, handler)
 }
 
 /**
- * 
+ *
  * 创建代理处理函数
- * @param {any} callback 
- * @returns 
+ * @param {any} callback
+ * @returns
  */
 function createApplyHandler(callback) {
-    return {
-        //对函数进行拦截
-        apply(target, ctx, args) {
-            callback(...[ctx].concat(args));
-            return Reflect.apply(...arguments);
-        }
-    }
+  return {
+    //对函数进行拦截
+    apply(target, ctx, args) {
+      callback(...[ctx].concat(args))
+      return Reflect.apply(...arguments)
+    },
+  }
 }
 
 /**
- * 
+ *
  * 观察方法
  * @export
- * @param {any} [addCallback=_callback] 
- * @param {any} [removeCallback=_callback] 
- * @returns 
+ * @param {any} [addCallback=_callback]
+ * @param {any} [removeCallback=_callback]
+ * @returns
  */
 export function watch(addCallback = _callback, removeCallback = _callback) {
+  function innerAddCallback() {
+    addCallback(...arguments)
+  }
 
-    function innerAddCallback() {
-        addCallback(...arguments);
-    }
+  function innerRemoveCallback() {
+    removeCallback(...arguments)
+  }
 
-    function innerRemoveCallback() {
-        removeCallback(...arguments);
-    }
+  if (isWatched) {
+    return
+  }
 
-    if (isWatched) {
-        return;
-    }
+  //创建可撤销代理，监听事件添加
+  rvAdd = createRevocableProxy(
+    ep.addEventListener,
+    createApplyHandler(innerAddCallback),
+  )
+  ep.addEventListener = rvAdd.proxy
 
-    //创建可撤销代理，监听事件添加
-    rvAdd = createRevocableProxy(ep.addEventListener, createApplyHandler(innerAddCallback));
-    ep.addEventListener = rvAdd.proxy;
-
-    //创建可撤销代理，监听事件移除
-    rvRemove = createRevocableProxy(ep.removeEventListener, createApplyHandler(innerRemoveCallback));
-    ep.removeEventListener = rvRemove.proxy;
+  //创建可撤销代理，监听事件移除
+  rvRemove = createRevocableProxy(
+    ep.removeEventListener,
+    createApplyHandler(innerRemoveCallback),
+  )
+  ep.removeEventListener = rvRemove.proxy
 }
 
 export function cancelWatch() {
-    //撤销添加事件的代理对象
-    rvAdd.revoke();
-    //撤销移除事件的代理对象
-    rvRemove.revoke();
-    //还原
-    ep.addEventListener = orgEventTargetPro.addEventListener;
-    ep.removeEventListener = orgEventTargetPro.removeEventListener;
+  //撤销添加事件的代理对象
+  rvAdd.revoke()
+  //撤销移除事件的代理对象
+  rvRemove.revoke()
+  //还原
+  ep.addEventListener = orgEventTargetPro.addEventListener
+  ep.removeEventListener = orgEventTargetPro.removeEventListener
 }
-
