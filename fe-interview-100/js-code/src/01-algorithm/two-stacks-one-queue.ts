6e074116a93973802cea6b64d4040fc4b7c2da1d
MineMine <gaoyuan_flying@163.com>
2023-06-16 23:41:33 +0800

    用两个栈实现一个队列

--- fe-interview-100/js-code/src/01-algorithm/two-stacks-one-queue.ts
+++ fe-interview-100/js-code/src/01-algorithm/two-stacks-one-queue.ts
@@ -4,50 +4,50 @@
  */
 
 export class MyQueue {
-    private stack1: number[] = []
-    private stack2: number[] = []
+    private stack1: number[] = [];
+    private stack2: number[] = [];
 
     /**
      * 入队
      * @param n n
      */
     add(n: number) {
-        this.stack1.push(n)
+        this.stack1.push(n);
     }
 
     /**
      * 出队
      */
     delete(): number | null {
-        let res
+        let res;
 
-        const stack1 = this.stack1
-        const stack2 = this.stack2
+        const stack1 = this.stack1;
+        const stack2 = this.stack2;
 
         // 将 stack1 所有元素移动到 stack2 中
-        while(stack1.length) {
-            const n = stack1.pop()
+        while (stack1.length) {
+            const n = stack1.pop();
             if (n != null) {
-                stack2.push(n)
+                stack2.push(n);
             }
         }
 
         // stack2 pop
-        res = stack2.pop()
+        res = stack2.pop();
 
         // 将 stack2 所有元素“还给”stack1
-        while(stack2.length) {
-            const n = stack2.pop()
+        while (stack2.length) {
+            const n = stack2.pop();
             if (n != null) {
-                stack1.push(n)
+                stack1.push(n);
             }
         }
 
-        return res || null
+        return res || null;
     }
 
     get length(): number {
-        return this.stack1.length
+        return this.stack1.length;
     }
 }
 
