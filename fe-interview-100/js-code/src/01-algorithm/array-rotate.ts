4c38ba70d71b683a77bc5ce176060dd54d7cb59a
MineMine <gaoyuan_flying@163.com>
2023-06-16 17:39:47 +0800

    旋转数组

--- fe-interview-100/js-code/src/01-algorithm/array-rotate.ts
+++ fe-interview-100/js-code/src/01-algorithm/array-rotate.ts
@@ -10,18 +10,18 @@
  * @returns arr
  */
 export function rotate1(arr: number[], k: number): number[] {
-    const length = arr.length
-    if (!k || length === 0) return arr
-    const step = Math.abs(k % length) // abs 取绝对值
+    const length = arr.length;
+    if (!k || length === 0) return arr;
+    const step = Math.abs(k % length); // abs 取绝对值
 
     // O(n^2)
     for (let i = 0; i < step; i++) {
-        const n = arr.pop()
+        const n = arr.pop();
         if (n != null) {
-            arr.unshift(n) // 数组是一个有序结构，unshift 操作非常慢！！！ O(n)
+            arr.unshift(n); // 数组是一个有序结构，unshift 操作非常慢！！！ O(n)
         }
     }
-    return arr
+    return arr;
 }
 
 /**
@@ -29,16 +29,16 @@ export function rotate1(arr: number[], k: number): number[] {
  * @param arr arr
  * @param k k
  */
- export function rotate2(arr: number[], k: number): number[] {
-    const length = arr.length
-    if (!k || length === 0) return arr
-    const step = Math.abs(k % length) // abs 取绝对值
+export function rotate2(arr: number[], k: number): number[] {
+    const length = arr.length;
+    if (!k || length === 0) return arr;
+    const step = Math.abs(k % length); // abs 取绝对值
 
     // O(1)
-    const part1 = arr.slice(-step) // O(1)
-    const part2 = arr.slice(0, length - step)
-    const part3 = part1.concat(part2)
-    return part3
+    const part1 = arr.slice(-step); // O(1)
+    const part2 = arr.slice(0, length - step);
+    const part3 = part1.concat(part2);
+    return part3;
 }
 
 // // 功能测试
