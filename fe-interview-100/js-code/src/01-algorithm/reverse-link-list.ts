5c032f1b0eb2e5954bd8e0bb61b35eb0d78e9f40
MineMine <gaoyuan_flying@163.com>
2023-06-18 09:48:31 +0800

    连环问：链表和数组，哪个实现队列更快

--- fe-interview-100/js-code/src/01-algorithm/reverse-link-list.ts
+++ fe-interview-100/js-code/src/01-algorithm/reverse-link-list.ts
@@ -4,8 +4,8 @@
  */
 
 export interface ILinkListNode {
-    value: number
-    next?: ILinkListNode
+    value: number;
+    next?: ILinkListNode;
 }
 
 /**
@@ -14,32 +14,32 @@ export interface ILinkListNode {
  */
 export function reverseLinkList(listNode: ILinkListNode): ILinkListNode {
     // 定义三个指针
-    let prevNode: ILinkListNode | undefined = undefined
-    let curNode: ILinkListNode | undefined = undefined
-    let nextNode: ILinkListNode | undefined = listNode
+    let prevNode: ILinkListNode | undefined = undefined;
+    let curNode: ILinkListNode | undefined = undefined;
+    let nextNode: ILinkListNode | undefined = listNode;
 
     // 以 nextNode 为主，遍历链表
-    while(nextNode) {
+    while (nextNode) {
         // 第一个元素，删掉 next ，防止循环引用
         if (curNode && !prevNode) {
-            delete curNode.next
+            delete curNode.next;
         }
 
         // 反转指针
         if (curNode && prevNode) {
-            curNode.next = prevNode
+            curNode.next = prevNode;
         }
 
         // 整体向后移动指针
-        prevNode = curNode
-        curNode = nextNode
-        nextNode = nextNode?.next
+        prevNode = curNode;
+        curNode = nextNode;
+        nextNode = nextNode?.next;
     }
 
     // 最后一个的补充：当 nextNode 空时，此时 curNode 尚未设置 next
-    curNode!.next = prevNode
+    curNode!.next = prevNode;
 
-    return curNode!
+    return curNode!;
 }
 
 /**
@@ -47,27 +47,27 @@ export function reverseLinkList(listNode: ILinkListNode): ILinkListNode {
  * @param arr number arr
  */
 export function createLinkList(arr: number[]): ILinkListNode {
-    const length = arr.length
-    if (length === 0) throw new Error('arr is empty')
+    const length = arr.length;
+    if (length === 0) throw new Error("arr is empty");
 
     let curNode: ILinkListNode = {
-        value: arr[length - 1]
-    }
-    if (length === 1) return curNode
+        value: arr[length - 1],
+    };
+    if (length === 1) return curNode;
 
     for (let i = length - 2; i >= 0; i--) {
         curNode = {
             value: arr[i],
-            next: curNode
-        }
+            next: curNode,
+        };
     }
 
-    return curNode
+    return curNode;
 }
 
-const arr = [100, 200, 300, 400, 500]
-const list = createLinkList(arr)
-console.info('list:', list)
+const arr = [100, 200, 300, 400, 500];
+const list = createLinkList(arr);
+console.info("list:", list);
 
-const list1 = reverseLinkList(list)
-console.info('list1:', list1)
+const list1 = reverseLinkList(list);
+console.info("list1:", list1);
