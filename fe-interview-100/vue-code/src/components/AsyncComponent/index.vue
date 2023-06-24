cdac19efaa0ce437fe4e222c59a7cc5d9e231dbd
MineMine <gaoyuan_flying@163.com>
2023-06-24 22:16:38 +0800

    优化

--- fe-interview-100/vue-code/src/components/AsyncComponent/index.vue
+++ fe-interview-100/vue-code/src/components/AsyncComponent/index.vue
@@ -1,14 +1,16 @@
 <template>
-    <Child></Child>
+  <Child></Child>
 </template>
 
 <script>
 import { defineAsyncComponent } from 'vue'
 
 export default {
-    name: 'AsyncComponent',
-    components: {
-        Child: defineAsyncComponent(() => import(/* webpackChunkName: "async-child" */ './Child.vue'))
-    }
+  name: 'AsyncComponent',
+  components: {
+    Child: defineAsyncComponent(() =>
+      import(/* webpackChunkName: "async-child" */ './Child.vue'),
+    ),
+  },
 }
 </script>
