e8e17b5e84c3e2ad3fe317aa03ef614256ab9217
MineMine <gaoyuan_flying@163.com>
2023-06-25 18:25:41 +0800

    手写bind

--- fe-interview-100/js-code/src/03-write-code/curry.ts
+++ fe-interview-100/js-code/src/03-write-code/curry.ts
@@ -4,26 +4,23 @@
  */
 
 export function curry(fn: Function) {
-    const fnArgsLength = fn.length // 传入函数的参数长度
-    let args: any[] = []
+    const fnArgsLength = fn.length; // 传入函数的参数长度
+    let args: any[] = [];
 
     // ts 中，独立的函数，this 需要声明类型
     function calc(this: any, ...newArgs: any[]) {
         // 积累参数
-        args = [
-            ...args,
-            ...newArgs
-        ]
+        args = [...args, ...newArgs];
         if (args.length < fnArgsLength) {
             // 参数不够，返回函数
-            return calc
+            return calc;
         } else {
             // 参数够了，返回执行结果
-            return fn.apply(this, args.slice(0, fnArgsLength))
+            return fn.apply(this, args.slice(0, fnArgsLength));
         }
     }
 
-    return calc
+    return calc;
 }
 
 // function add(a: number, b: number, c: number): number {
