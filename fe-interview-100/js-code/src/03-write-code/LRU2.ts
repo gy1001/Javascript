02fe296b03b65e45bbade82034fe05c0c674e55e
MineMine <gaoyuan_flying@163.com>
2023-06-26 14:56:33 +0800

    LRU缓存

--- fe-interview-100/js-code/src/03-write-code/LRU2.ts
+++ fe-interview-100/js-code/src/03-write-code/LRU2.ts
@@ -4,120 +4,120 @@
  */
 
 interface IListNode {
-    value: any
-    key: string // 存储 key ，方便删除（否则删除时就需要遍历 this.data )
-    prev?: IListNode
-    next?: IListNode
+    value: any;
+    key: string; // 存储 key ，方便删除（否则删除时就需要遍历 this.data )
+    prev?: IListNode;
+    next?: IListNode;
 }
 
 export default class LRUCache {
-    private length: number
-    private data: { [key: string]: IListNode } = {}
-    private dataLength: number = 0
-    private listHead: IListNode | null = null
-    private listTail: IListNode | null = null
+    private length: number;
+    private data: { [key: string]: IListNode } = {};
+    private dataLength: number = 0;
+    private listHead: IListNode | null = null;
+    private listTail: IListNode | null = null;
 
     constructor(length: number) {
-        if (length < 1) throw new Error('invalid length')
-        this.length = length
+        if (length < 1) throw new Error("invalid length");
+        this.length = length;
     }
 
     private moveToTail(curNode: IListNode) {
-        const tail = this.listTail
-        if (tail === curNode) return
+        const tail = this.listTail;
+        if (tail === curNode) return;
 
         // -------------- 1. 让 prevNode nextNode 断绝与 curNode 的关系 --------------
-        const prevNode = curNode.prev
-        const nextNode = curNode.next
+        const prevNode = curNode.prev;
+        const nextNode = curNode.next;
         if (prevNode) {
             if (nextNode) {
-                prevNode.next = nextNode
+                prevNode.next = nextNode;
             } else {
-                delete prevNode.next
+                delete prevNode.next;
             }
         }
         if (nextNode) {
             if (prevNode) {
-                nextNode.prev = prevNode
+                nextNode.prev = prevNode;
             } else {
-                delete nextNode.prev
+                delete nextNode.prev;
             }
 
-            if (this.listHead === curNode) this.listHead = nextNode
+            if (this.listHead === curNode) this.listHead = nextNode;
         }
 
         // -------------- 2. 让 curNode 断绝与 prevNode nextNode 的关系 --------------
-        delete curNode.prev
-        delete curNode.next
+        delete curNode.prev;
+        delete curNode.next;
 
         // -------------- 3. 在 list 末尾重新建立 curNode 的新关系 --------------
         if (tail) {
-            tail.next = curNode
-            curNode.prev = tail
+            tail.next = curNode;
+            curNode.prev = tail;
         }
-        this.listTail = curNode
+        this.listTail = curNode;
     }
 
     private tryClean() {
         while (this.dataLength > this.length) {
-            const head = this.listHead
-            if (head == null) throw new Error('head is null')
-            const headNext = head.next
-            if (headNext == null) throw new Error('headNext is null')
+            const head = this.listHead;
+            if (head == null) throw new Error("head is null");
+            const headNext = head.next;
+            if (headNext == null) throw new Error("headNext is null");
 
             // 1. 断绝 head 和 next 的关系
-            delete headNext.prev
-            delete head.next
+            delete headNext.prev;
+            delete head.next;
 
             // 2. 重新赋值 listHead
-            this.listHead = headNext
+            this.listHead = headNext;
 
             // 3. 清理 data ，重新计数
-            delete this.data[head.key]
-            this.dataLength = this.dataLength - 1
+            delete this.data[head.key];
+            this.dataLength = this.dataLength - 1;
         }
     }
 
     get(key: string): any {
-        const data = this.data
-        const curNode = data[key]
+        const data = this.data;
+        const curNode = data[key];
 
-        if (curNode == null) return null
+        if (curNode == null) return null;
 
         if (this.listTail === curNode) {
             // 本身在末尾（最新鲜的位置），直接返回 value
-            return curNode.value
+            return curNode.value;
         }
 
         // curNode 移动到末尾
-        this.moveToTail(curNode)
+        this.moveToTail(curNode);
 
-        return curNode.value
+        return curNode.value;
     }
 
     set(key: string, value: any) {
-        const data = this.data
-        const curNode = data[key]
+        const data = this.data;
+        const curNode = data[key];
 
         if (curNode == null) {
             // 新增数据
-            const newNode: IListNode = { key, value }
+            const newNode: IListNode = { key, value };
             // 移动到末尾
-            this.moveToTail(newNode)
+            this.moveToTail(newNode);
 
-            data[key] = newNode
-            this.dataLength++
+            data[key] = newNode;
+            this.dataLength++;
 
-            if (this.dataLength === 1) this.listHead = newNode
+            if (this.dataLength === 1) this.listHead = newNode;
         } else {
             // 修改现有数据
-            curNode.value = value
+            curNode.value = value;
             // 移动到末尾
-            this.moveToTail(curNode)
+            this.moveToTail(curNode);
         }
 
         // 尝试清理长度
-        this.tryClean()
+        this.tryClean();
     }
 }
 
