dcf394b97f84c8ae53d2b0a3fc5f5e5a76e8d0f5
MineMine <gaoyuan_flying@163.com>
2023-06-30 14:57:03 +0800

    vue基本使用 +高级特性

--- frame-project-interview/vue-code-demo/src/components/AdvancedUse/mixin.js
+++ frame-project-interview/vue-code-demo/src/components/AdvancedUse/mixin.js
@@ -1,17 +1,17 @@
 export default {
-    data() {
-        return {
-            city: '北京'
-        }
-    },
-    methods: {
-        showName() {
-            // eslint-disable-next-line
-            console.log(this.name)
-        }
-    },
-    mounted() {
-        // eslint-disable-next-line
-        console.log('mixin mounted', this.name)
+  data() {
+    return {
+      city: '北京',
     }
+  },
+  methods: {
+    showName() {
+      // eslint-disable-next-line
+      console.log(this.name)
+    },
+  },
+  mounted() {
+    // eslint-disable-next-line
+    console.log('mixin mounted', this.name)
+  },
 }
