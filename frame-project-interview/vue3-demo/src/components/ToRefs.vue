409ba7148e35a0dd45eb386bc245bae229b12439
MineMine <gaoyuan_flying@163.com>
2023-07-06 00:31:31 +0800

    ref

--- frame-project-interview/vue3-demo/src/components/ToRefs.vue
+++ frame-project-interview/vue3-demo/src/components/ToRefs.vue
@@ -1,31 +1,31 @@
 <template>
-    <p>toRefs demo {{age}} {{name}}</p>
+  <p>toRefs demo {{ age }} {{ name }}</p>
 </template>
 
 <script>
 import { ref, toRef, toRefs, reactive } from 'vue'
 
 export default {
-    name: 'ToRefs',
-    setup() {
-        const state = reactive({
-            age: 20,
-            name: '双越'
-        })
+  name: 'ToRefs',
+  setup() {
+    const state = reactive({
+      age: 20,
+      name: '双越',
+    })
 
-        const stateAsRefs = toRefs(state) // 将响应式对象，变成普通对象
+    const stateAsRefs = toRefs(state) // 将响应式对象，变成普通对象
 
-        // const { age: ageRef, name: nameRef } = stateAsRefs // 每个属性，都是 ref 对象
-        // return {
-        //     ageRef,
-        //     nameRef
-        // }
+    // const { age: ageRef, name: nameRef } = stateAsRefs // 每个属性，都是 ref 对象
+    // return {
+    //     ageRef,
+    //     nameRef
+    // }
 
-        setTimeout(() => {
-            state.age = 25
-        }, 1500)
+    setTimeout(() => {
+      state.age = 25
+    }, 1500)
 
-        return stateAsRefs
-    }
+    return stateAsRefs
+  },
 }
 </script>
