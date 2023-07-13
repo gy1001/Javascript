8024cd300031f616b502ddc926333265b2a48ad8
MineMine <gaoyuan_flying@163.com>
2023-07-14 00:27:23 +0800

    项目设计

--- frame-project-interview/react-code-demo/src/components/TodoLIst/List.js
+++ frame-project-interview/react-code-demo/src/components/TodoLIst/List.js
@@ -2,14 +2,18 @@ import React from 'react'
 import ListItem from './ListItem'
 
 function List({ list = [], deleteItem, toggleCompleted }) {
-    return <div>
-        {list.map(item => <ListItem
-            item={item}
-            key={item.id}
-            deleteItem={deleteItem}
-            toggleCompleted={toggleCompleted}
-        />)}
+  return (
+    <div>
+      {list.map((item) => (
+        <ListItem
+          item={item}
+          key={item.id}
+          deleteItem={deleteItem}
+          toggleCompleted={toggleCompleted}
+        />
+      ))}
     </div>
+  )
 }
 
 export default List
