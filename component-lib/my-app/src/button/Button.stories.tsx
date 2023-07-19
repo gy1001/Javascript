91e41339261aefc4d1cbae404709c648d919c446
MineMine <gaoyuan_flying@163.com>
2023-07-19 23:52:37 +0800

    移动目录并导出ButtonGroup

--- component-lib/my-app/src/button/Button.stories.tsx
+++ component-lib/my-app/src/button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import Button from './Button'
+import Button from './index'
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
@@ -21,13 +21,31 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+// // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
   args: {
     // primary: true,
     type: 'primary',
     children: 'Button',
   },
+}
+const style = { marginLeft: '8px' }
+export const ButtonGroup: Story = {
+  render: () => (
+    <>
+      <Button type='primary'>Primary Button</Button>
+      <Button style={style}>Default Button</Button>
+      <Button type='dashed' style={style}>
+        Dashed Button
+      </Button>
+      <Button type='text' style={style}>
+        Text Button
+      </Button>
+      <Button type='link' style={style}>
+        Link Button
+      </Button>
+    </>
+  ),
 }
 
 // export const Secondary: Story = {
--- component-lib/my-app/src/button/index.tsx
+++ component-lib/my-app/src/button/index.tsx
@@ -5,12 +5,13 @@ import './index.css'
 
 interface ButtonProps {
   className?: string
-  type?: 'primary' | 'dangerous'
+  type?: 'primary' | 'dangerous' | 'dashed' | 'text' | 'link'
   children?: ReactNode
+  style?: React.CSSProperties
 }
 
 const Button = (props: ButtonProps) => {
-  const { className, type, children } = props
+  const { className, type, children, style } = props
   const classObj = {
     'ant-btn': true,
     [`ant-btn-${type}`]: type,
@@ -19,7 +20,11 @@ const Button = (props: ButtonProps) => {
     classObj[className] = true
   }
   const cls = classNames(classObj)
-  return <button className={cls}>{children}</button>
+  return (
+    <button className={cls} style={style}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
