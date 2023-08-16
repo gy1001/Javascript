2d0498357b84c909b5872e3c4faf7e870ad63fa1
高原 <gaoyuan200408@credithc.com>
2023-08-16 18:35:31 +0800

    feat:匹配器

--- TDD-BDD/lesson-02/math.test.js
+++ TDD-BDD/lesson-02/math.test.js
@@ -7,3 +7,45 @@ test("测试加法 3 + 7", ()=>{
 test("测试减法 6 - 3", ()=>{
   expect(minus(6, 3)).toBe(3)
 })
+
+test('null', () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
+
+
+const fetchData =function () {
+  return new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      resolve('peanut butter')
+    }, 3000)
+  })
+}
+
+
+test('the data is peanut butter', async () => {
+  const data = await fetchData();
+  expect(data).toBe('peanut butter');
+});
+
+test('the fetch fails with an error', async () => {
+  expect.assertions(0);
+  try {
+    await fetchData();
+  } catch (e) {
+    expect(e).toMatch('error');
+  }
+});
