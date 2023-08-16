3357a5b80c6fde0270396d8901b9cc17522297c7
高原 <gaoyuan200408@credithc.com>
2023-08-16 09:36:16 +0800

    feat:增加babel识别es module 语法

--- TDD-BDD/lesson-02/math.js
+++ TDD-BDD/lesson-02/math.js
@@ -1,12 +1,19 @@
-function add(a, b) {
-  return a + b
+// function add(a, b) {
+//   return a + b
+// }
+//
+// function minus(a, b) {
+//   return a - b
+// }
+//
+// module.exports = {
+//   add,
+//   minus
+// }
+export function add(a, b) {
+ return a + b
 }
 
-function minus(a, b) {
-  return a - b
-}
-
-module.exports = {
-  add,
-  minus
+export function minus(a, b) {
+ return a - b
 }
--- TDD-BDD/lesson-02/math.test.js
+++ TDD-BDD/lesson-02/math.test.js
@@ -1,4 +1,5 @@
-const { add, minus }  = require("./math")
+// const { add, minus }  = require("./math")
+import { add, minus } from './math'
 test("测试加法 3 + 7", ()=>{
   expect(add(3, 7)).toBe(10)
 })
