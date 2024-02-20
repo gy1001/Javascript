6fac0591f2cf559d44aa6c11cd5b057b819e5e09
高原 <gaoyuan_flying@163.com>
2024-02-20 21:32:33 +0800

    feat:命令行问答交互

--- Node-Cli/lib/action.js
+++ Node-Cli/lib/action.js
@@ -1,7 +1,24 @@
+const inquirer = require('inquirer')
+const { framework } = require('../config')
 const createAction = (project, other) => {
-  console.log(project, other)
+  // console.log(project, other)
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'framework',
+        choices: framework,
+        message: '请选择你要使用的框架',
+      },
+    ])
+    .then((answers) => {
+      console.log('then', answers)
+    })
+    .catch((error) => {
+      console.log('catch', error)
+    })
 }
 
 module.exports = {
-  createAction
+  createAction,
 }
